54eb708b49e8122cff9d9f45e229a37198a0b186
elCuervo <yo@brunoaguirre.com>
2016-08-10 15:10:09 -0300

    Fail if I can't access a socket

--- gadget.go
+++ gadget.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
 )
 
 var addr = flag.String("address", ":53", "Address to bind.")
@@ -11,6 +12,10 @@ var socket = flag.String("socket", "/var/run/docker.sock", "Docker socket")
 
 func main() {
 	flag.Parse()
+
+	if _, err := os.Stat(*socket); os.IsNotExist(err) {
+		log.Fatal("I need access to the Docker socket to be able to work.")
+	}
 
 	server := NewDNSServer(*addr, *domain)
 	lookup := NewContainerLookup(*socket)
